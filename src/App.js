f4c060b22b792871997c5d3c156d9b4523278788
Sarah Yun <sarahyun7@gmail.com>
2019-06-30 01:56:23 -0400

    useState + useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [name, setName] = useState('Sarah')
+  const [age, setAge] = useState('24')
+
+  useEffect(() => {
+    document.title = name
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, I'm {name}</h1>
+      <button onClick={() => setName('Spiderman')}>
+        Change Name
+      </button>
+      <h2>I am {age} years old</h2>
+      <button onClick={() => setAge(age-1)}>Younger</button>
+      <button onClick={() => setAge(age+1)}>Older</button>
     </div>
   );
 }
 
 export default App;
+
+// useState
+  // returns a stateful value and a function to update it (compared to initalizing apps separately and using setState function to update state)
+
+  // imported useState hook from React
+  // created constant that returned a pair of objects, name & setName from useState
+  // initialized useState hook to value of 'Sarah'
+  // used name property instead of a hard-coded value
+  // called setName function to change the value of name on click of button
+
+
+// useEffect
+  // takes a function that contains code (fetching data or subscribing to a service) and returns a function that will be executed before the effect runs and when the component is unmounted
+  // re-initializes itself from the last iteration
+  
+  // imported useEffect
+  // used this effect to change the title of the HTML document
+  // dynamically changes value of document.title every time the name state property changes
+  // executes first when the component is done loading on to the page, then again when the state of the component gets updated
